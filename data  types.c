ff38745a3784cbff62e03d0393dd1dc0a0c041dd
vaishnavi-codesai <saivaishnavi1511@gmail.com>
2026-06-16 22:49:47 +0530

    Add files via upload
    added c program on character data types

---
+++ data  types.c
@@ -0,0 +1,12 @@
+//programme on data types
+#include <stdio.h>
+int main()
+{
+  char a,b,c;
+  a= 'v';
+  b= 's';
+  c= 'm';
+  printf("the character is %c\n",a);
+  printf("the character is %5c\n",b);
+  printf("the character is %8c",c);
+}
