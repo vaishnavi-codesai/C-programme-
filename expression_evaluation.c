9ee422f2840c4bfcb2ea1c1fc3b0d9237dc597aa
vaishnavi-codesai <saivaishnavi1511@gmail.com>
2026-06-17 18:16:51 +0530

    programme on long expression evaluation

---
+++ expression_evaluation.c
@@ -0,0 +1,12 @@
+//example on expression evaluation
+#include <stdio.h>
+int main()
+{ float l,m,n,o,res;
+   l=75;
+   m=25;
+   n=30;
+   o=5;
+   res=(l-m+n)*o;
+   printf("result is %d",res);
+   
+}
