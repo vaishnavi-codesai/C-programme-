d2df748ebb3cbf82a223410d88a885ffc18d68ce
vaishnavi-codesai <saivaishnavi1511@gmail.com>
2026-06-17 14:27:48 +0530

    added c program for arthimetic operations

---
+++ arthimetic operations.c
@@ -0,0 +1,16 @@
+ //example on arthimetic operations
+ #include <stdio.h>
+ void main()
+ {  
+   int a,b,res;
+   a=10;
+   b=5;
+   res=a+b;
+   printf("the sum is %d\n",res);
+   res=a-b;
+   printf("the difference is %d\n",res);
+   res=a*b;
+   printf("the product is %d\n",res);
+   res=a/b;
+   printf("the division is %d\n",res);
+}
