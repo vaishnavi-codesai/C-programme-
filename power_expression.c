e74100db227a50b2cbd11e8ea2a04873074252e6
vaishnavi-codesai <saivaishnavi1511@gmail.com>
2026-06-21 14:50:29 +0530

    programme on power expresssion evaluation with the input statement of scanf statement

---
+++ power_expression.c
@@ -0,0 +1,10 @@
+ //calculate the exprression a^2+b^2-2ab
+ #include <stdio.h>
+ #include <math.h>
+ void main()
+ { int a,b,res;
+    printf("enter any two values ");
+    scanf("%d%d",&a,&b);
+    res=pow(a,2)+pow(b,2)-2*a*b
+    printf("\n result is %d",res);
+ }   
