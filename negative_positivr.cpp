92c4da0a9c5a22f443462f5b13fee03c5e233cd2
vaishnavi-codesai <saivaishnavi1511@gmail.com>
2026-06-24 12:17:32 +0530

    code to identify negative numbers and positive numbers

---
+++ negative_positivr.cpp
@@ -0,0 +1,12 @@
+/*accept any number and display positive num or negative number*/
+#include <stdio.h>
+int main()
+{int num;
+   printf(" enter any number ");
+   scanf("%d",&num);
+
+    if(num>=0)
+    printf("num is positive");
+    else
+    printf("num is neagative");
+}
