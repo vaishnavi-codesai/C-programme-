568ca70eba905be9bd5d6e27d9bcf8e1d2f95789
vaishnavi-codesai <saivaishnavi1511@gmail.com>
2026-06-24 12:13:35 +0530

    programme to identify equal or unequal numbers

---
+++ equal_unequal.c
@@ -0,0 +1,13 @@
+/*accept any two numbers and display eaual or uneaual number*/
+#include <stdio.h>
+int main()
+{int a,b,equal;
+     printf("enter any two numbers ");
+     scanf("%d%d",&a,&b);
+	    
+		
+     if(a==b)
+	 printf("both numbers are equal");
+	 else
+	 printf("both numbers are unequal");    
+}
