36fc985e62b7a7144eb404e842a814bed4aba562
vaishnavi-codesai <saivaishnavi1511@gmail.com>
2026-06-24 12:21:04 +0530

    programme to exchange or swap numbers by c programme

---
+++ swaping_the_digits.c
@@ -0,0 +1,16 @@
+/*accept any two digit number and swap them*/
+#include <stdio.h>
+#include <math.h>
+int main()
+{ 
+  int a,temp;
+  printf("enter any two digit number ");
+  scanf("%d",&a);
+  temp=a;
+  a=temp;
+  printf("a=%d",a);
+  
+  
+  
+}
+
