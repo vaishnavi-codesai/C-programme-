bd9f0759bab5ffcb9c168aaac502c789f4da857b
vaishnavi-codesai <saivaishnavi1511@gmail.com>
2026-06-24 12:18:43 +0530

    code to identify odd  and even numbers by if else statement

---
+++ odd_even.c
@@ -0,0 +1,13 @@
+/*accept any number odd or even*/
+#include <stdio.h>
+int main()
+{int num;
+     printf("enter any number ");
+     scanf("%d",&num);
+      
+      
+      if(num%2==0)
+      printf("even num");
+      else
+     printf("odd num ");
+}
